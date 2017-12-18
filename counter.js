4449bb2ed03ebebfe2ec405ffe556032c4e9443b
wqmei <314216221@qq.com>
2017-12-18 11:31:03 +0800

    修改计时器

--- counter.js
+++ counter.js
@@ -7,6 +7,22 @@ document.getElementById('txt').value=c
 c=c+1
 t=setTimeout("timedCount()",1000)
 }
+function time(controlButton)
+{
+    var checkFlag = parseInt(controlButton.getAttribute("checkFlag"));
+    //为0代表停止状态
+    if(checkFlag==0)
+    {
+        controlButton.value = "停止计时";
+        controlButton.setAttribute("checkFlag", 1);
+        timedCount();
+    }else
+    {
+        controlButton.value = "开始计时";
+        controlButton.setAttribute("checkFlag", 0);
+        stopCount();
+    }
+}
 
 function stopCount()
 {
